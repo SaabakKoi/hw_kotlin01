811b134ff45b5fbca496bed781f2f1e5f1717182
Max <Max>
2024-06-10 12:24:41 +0300

    corrections for hw

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,35 +3,41 @@ fun main(args: Array<String>) {
     do {
         println("Choose command")
         val command = readlnOrNull()
-        if(command=="add"){
-            println("Enter name:")
-            val name = readlnOrNull()
-            println("Enter phone number or Email")
-            val phoneOrEmail = readlnOrNull()
-            println(addPerson(name.toString(), phoneOrEmail.toString()))
+        val splitCommand = command.toString().split(" ")
+        if (splitCommand.get(0) == "add") {
+            println(addContact(command))
+        }
+        if (splitCommand.get(0) == "help") {
+            println(
+                "Command 'add' : Adding new contact with name and phone number or email adress.\n" +
+                        "EXAMPLE: add John phone +7895646 or add John email myEmail@mail.ru \n" +
+                        "Command 'help' : Showing available commands and their purpose\n" +
+                        "Command 'exit' :Exiting from app\n"
+            )
 
         }
-        if (command=="help"){
-            println("Command 'add' : Adding new contact with name and phone number or email adress.\n" +
-                    "Command 'help' : Showing available commands and their purpose\n" +
-                    "Command 'exit' :Exiting from app\n")
 
-        }
-
-    } while (command!="exit")
+    } while (command != "exit")
 
 
 }
 
-fun addPerson(name:String, phoneOrEmail:String): String {
-
+fun addContact(line: String?): String {
+    val myLine = line.toString().split(" ")
     val regexName = """([a-zA-Z])+""".toRegex()
     val regex = """([+, 0-9])+""".toRegex()
     val regexEmail = """([a-zA-Z0-9])+\@([a-z])+\.([a-z])+""".toRegex()
-    if (regexName.matches(name)&&regex.matches(phoneOrEmail))
-    return "$name $phoneOrEmail"
-    if (regexName.matches(name)&&regexEmail.matches(phoneOrEmail))
-        return "$name $phoneOrEmail"
-
+    if (regexName.matches(myLine.get(1))) {
+        if (regex.matches(myLine.get(3))&&myLine.get(2)=="phone") {
+            val name = myLine.get(1)
+            val phone = myLine.get(3)
+            return "$name $phone"
+        }
+        if (regexEmail.matches(myLine.get(3))&&myLine.get(2)=="email") {
+            val name = myLine.get(1)
+            val email = myLine.get(3)
+            return "$name $email"
+        }
+    }
     return "wrong entering"
 }
